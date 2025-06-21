5c0b74195e604e71175810791dfbd47f27423b0d
Norepinephrine_code <hadrielpogi@gmail.com>
2025-06-21 19:12:22 +0800

    Indicate first column as House

--- GameBoard.java
+++ GameBoard.java
@@ -34,34 +34,43 @@ public class GameBoard {
      * "---" represents an empty tile, "S" is a {@link Sunflower},
      * "P" a {@link Peashooter}. Zombie tiles show one of:
      * "ZN" for {@link NormalZombie}, "ZF" for {@link FlagZombie},
-     * or "ZC" for {@link ConeheadZombie}.
+     * or "ZC" for {@link ConeheadZombie}. Multiple zombies show " M ".
+     * Column 0 always shows " H " to indicate the player's house.
      */
     public void display() {
         System.out.println("\n======= Current Game Board  =======");
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                Tile tile = board[r][c];
-                String content = "---";
-                Plant p = tile.getPlant();
-                boolean hasZombie = tile.hasZombies();
+                String content;
 
-                if (hasZombie) {
-                    if (tile.getZombies().size() > 1) {
-                        content = " M ";
-                    } else {
-                        Zombie z = tile.getZombies().get(0);
-                        if (z instanceof FlagZombie) {
-                            content = "ZF ";
-                        } else if (z instanceof ConeheadZombie) {
-                            content = "ZC ";
+                if (c == 0) {
+                    // Always show the house symbol on column 0
+                    content = " H ";
+                } else {
+                    Tile tile = board[r][c];
+                    Plant p = tile.getPlant();
+                    boolean hasZombie = tile.hasZombies();
+
+                    if (hasZombie) {
+                        if (tile.getZombies().size() > 1) {
+                            content = " M ";
                         } else {
-                            content = "ZN ";
+                            Zombie z = tile.getZombies().get(0);
+                            if (z instanceof FlagZombie) {
+                                content = "ZF ";
+                            } else if (z instanceof ConeheadZombie) {
+                                content = "ZC ";
+                            } else {
+                                content = "ZN ";
+                            }
                         }
+                    } else if (p instanceof Sunflower) {
+                        content = " S ";
+                    } else if (p instanceof Peashooter) {
+                        content = " P ";
+                    } else {
+                        content = "---";
                     }
-                } else if (p instanceof Sunflower) {
-                    content = " S ";
-                } else if (p instanceof Peashooter) {
-                    content = " P ";
                 }
 
                 System.out.print(content);
