811a7e01806316561ebf896ebb0f3852cf3b9acc
Norepinephrine-code <75923243+Norepinephrine-code@users.noreply.github.com>
2025-06-21 12:49:57 +0800

    Update Zombie.java
    Completed the getters and setters

--- zombies/Zombie.java
+++ zombies/Zombie.java
@@ -1,5 +1,7 @@
 package zombies;
+
 import tiles.Tile;
+
 public abstract class Zombie {
     protected int health = 70;
     protected int baseSpeed = 4;
@@ -22,6 +24,8 @@ public abstract class Zombie {
 
     public abstract void move();
 
+    public abstract void attack();
+
     public void takeDamage(int amount) {
         health -= amount;
     }
@@ -40,13 +44,57 @@ public abstract class Zombie {
         }
     }
 
-    public abstract void attack();
-
     public boolean isAlive() {
         return health > 0;
     }
 
+    // Getters
     public Tile getPosition() {
         return position;
+    }
+
+    public int getBaseSpeed() {
+        return baseSpeed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public boolean isFrozen() {
+        return isFreeze;
+    }
+
+    public int getFreezeTimer() {
+        return freezeTimer;
+    }
+
+    // Setters
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setPosition(Tile position) {
+        this.position = position;
+    }
+
+    public void setFrozen(boolean frozen) {
+        this.isFreeze = frozen;
+    }
+
+    public void setFreezeTimer(int freezeTimer) {
+        this.freezeTimer = freezeTimer;
     }
 }
