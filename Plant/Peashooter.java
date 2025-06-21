7a7aa770740f0276f9633ab20f0d461ef0789423
Norepinephrine-code <75923243+Norepinephrine-code@users.noreply.github.com>
2025-06-21 11:03:12 +0800

    Add files via upload

---
+++ Plant/Peashooter.java
@@ -0,0 +1,8 @@
+package MCO1_Files.Plant;
+
+public class Peashooter extends Plant {
+
+    Public Peashooter(Tile position){
+        super(100, 2.5, 15, 300, 9, 20, 2, position);
+    }
+}
---
+++ Zombie/Zombie.java
@@ -0,0 +1,50 @@
+public abstract class Zombie {
+    protected int health = 70;
+    protected int baseSpeed = 4;
+    protected int speed = 4;
+    protected int damage = 10;
+    protected Tile position;
+
+    protected boolean isFreeze;
+    protected int freezeTimer;
+
+    public Zombie(int h, int bs, int d, Tile pos) {
+        this.health = h;
+        this.baseSpeed = bs;
+        this.speed = bs;
+        this.damage = d;
+        this.position = pos;
+        this.isFreeze = false;
+        this.freezeTimer = 0;
+    }
+
+    public abstract void move();
+
+    public void takeDamage(int amount) {
+        health -= amount;
+    }
+
+    public void freezeFor(int ticks) {
+        isFreeze = true;
+        freezeTimer = ticks;
+    }
+
+    public void update() {
+        if (isFreeze) {
+            freezeTimer--;
+            if (freezeTimer <= 0) {
+                isFreeze = false;
+            }
+        }
+    }
+
+    public abstract void attack();
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public Tile getPosition() {
+        return position;
+    }
+}
