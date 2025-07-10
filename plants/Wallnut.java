0580898291bde9e08f4277ffd6f8fa0e10f23ee5
Norepinephrine_code <hadrielpogi@gmail.com>
2025-07-10 12:31:29 +0800

    Added Wall fucking nut

---
+++ plants/Wallnut.java
@@ -0,0 +1,14 @@
+package plants;
+import tiles.Tile;
+import zombies.Zombie;
+
+public class Wallnut extends Plant {
+
+    public Wallnut(Tile pos) {
+        super(50, 30.f,0,200, 0,0,0,pos);
+    }
+    @Override
+    public int action(Zombie zombie) {
+        // Do nothing 
+    }
+}
